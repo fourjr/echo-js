d32ba4dba74c5fbdea9a571701b7e80c36538e85
fourjr <yeejiarong2004@gmail.com>
2017-12-25 13:47:17 +0700

    raw the rest

--- economy.js
+++ economy.js
@@ -4,17 +4,17 @@ orange = "5AADFF";
 
 function refreshAccount(){
   try{
-    bank = JSON.parse(economy[UserID]);
-    bank.name = Username;
+    bank = JSON.parse(economy[RawUserID]);
+    bank.name = RawUsername;
   } catch (e) {
     def = {name:Username, money:0, cooldown:0};
-    economy[UserID] = JSON.stringify(def);
+    economy[RawUserID] = JSON.stringify(def);
   }
   return;
 }
 
 function checkCooldown(){
-  bank = JSON.parse(economy[UserID])
+  bank = JSON.parse(economy[RawUserID])
   unix = Math.round((new Date()).getTime() / 1000);
   if(bank.cooldown < unix){
     return false;
@@ -32,30 +32,27 @@ return hDisplay + mDisplay + sDisplay;
 }
 
 function startCooldown(time){
-  bank = JSON.parse(economy[RawUserID]);
+  bank = JSON.parse(economy[UserID]);
   unix = Math.round((new Date()).getTime() / 1000);
   bank.cooldown = unix + 60*60*time;
-  economy[UserID] = JSON.stringify(bank);
+  economy[RawUserID] = JSON.stringify(bank);
   return;
 }
 
 function editMoney(tare, value){
-  bank = JSON.parse(economy[RawUserID]) 
+  bank = JSON.parse(economy[UserID]) 
   if(tare === 'positive'){
     bank.money += value;
   }
   else if(tare === 'negative'){
     bank.money -= value;
   }
-  economy[RawUserID] = JSON.stringify(bank);
+  economy[UserID] = JSON.stringify(bank);
   return;
 } 
 
-function getAccount(mention){
-  if mention:
-    return JSON.parse(economy[UserID]);
-  else:
-    return JSON.parse(economy[RawUserID]);
+function getAccount(){
+  return JSON.parse(economy[UserID]);
 }
 
 function formatEmbed(title, body, color){
@@ -63,7 +60,7 @@ function formatEmbed(title, body, color){
   emb.title = title;
   emb.description = body;
   emb.color = HTML2Int("#" + color);
-  emb.author = {name:RawUsername, icon_url: "https://cdn.discordapp.com/avatars/" + RawUserID + "/" + JSON.stringify(RawUserImage).replace(/"/g, "") + ".webp?size=1024"};
+  emb.author = {name:Username, icon_url: "https://cdn.discordapp.com/avatars/" + UserID + "/" + JSON.stringify(UserImage).replace(/"/g, "") + ".webp?size=1024"};
   return emb;
 }
 
