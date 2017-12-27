5aad8f1660aacca3d0a3bba9a7a8883a5e687362
fourjr <yeejiarong2004@gmail.com>
2017-12-27 10:34:01 +0700

    add more $__

--- economy.js
+++ economy.js
@@ -4,11 +4,11 @@ green = "00FF00"; red = "FF4500"; orange= "FFC800"; blue = "5AADFF";
 
 reserved = ['<@249891250117804032>', Prefix];
 
-work = ["You found $__ on the floor!",   "Your boss decided to give you a bonus of $__ as you were working hard!",   "You worked as a chef and earned $ !",   "You had a successful business proposal and earned $ from it!" ];
+work = ["You found $__ on the floor!",   "Your boss decided to give you a bonus of $__ as you were working hard!",   "You worked as a chef and earned $__!",   "You had a successful business proposal and earned $__ from it!" ];
 
-robpos = ["You broke into someone**+**'s house and stole his $__", "You rob a random passerby**+** and manage to steal $__!", "You saw a bank and seized the opportunity. You earned $__ from the heist!", "You hacked Echo and gave yourself $__", "You bribed a staff member to add $__ to your account and it worked!"];
+robpos = ["You broke into someone's house and stole his $__", "You rob a random passerby and manage to steal $__!", "You saw a bank and seized the opportunity. You earned $__ from the heist!", "You hacked Echo and gave yourself $__", "You bribed a staff member to add $__ to your account and it worked!"];
 
-robneg = ["You try to rob a shop but got caught and fined $ ",   "You were caught vandalizing and got fined $ ",   "You hacked into the white house database but got caught. You lose $ ",   "You were caught stealing cookies and got fined $ ",   "You lost a bet against someone**+** and lost $__" ];
+robneg = ["You try to rob a shop but got caught and fined $__",   "You were caught vandalizing and got fined $__",   "You hacked into the white house database but got caught. You lose $__",   "You were caught stealing cookies and got fined $__",   "You lost a bet against someone and lost $__" ];
 
 helpMessage = "```md\
 Important Notes:\
