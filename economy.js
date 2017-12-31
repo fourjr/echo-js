8249f9eafa297ad89ec41f1f0c199fd56cedf742
fourjr <yeejiarong2004@gmail.com>
2017-12-31 13:13:00 +0700

    Make it & for last thing in time

--- economy.js
+++ economy.js
@@ -221,7 +221,8 @@ function checkCooldown() {
     hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
     mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
     sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return hDisplay + mDisplay + sDisplay;
+    Display = hDisplay + mDisplay + sDisplay;
+    return Display.replace(,([^,]*)$, " and $1");
   }
 }
 
