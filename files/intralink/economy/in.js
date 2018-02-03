f2d0fae109b7a137a75593f0ddc9575e074b8d64
Jia Rong Yee <yeejiarong2004@gmail.com>
2018-02-03 17:46:53 +0800

    Move all into a files folder, add checkPrefix to start of all rules

--- files/intralink/economy/in.js
+++ files/intralink/economy/in.js
@@ -10,7 +10,7 @@ reserved = ["<@249891250117804032>", "."];
 
 work = ["You found $__ on the floor!", "Your boss decided to give you a bonus of $__ as you were working hard!", "You worked as a chef and earned $__!", "You had a successful business proposal and earned $__ from it!"];
 
-robpos = ["You broke into someone's house and stole his $__", "You rob a random passerby and manage to steal $__!", "You saw a bank and seized the opportunity. You earned $__ from the heist!", "You hacked Echo and gave yourself $__", "You bribed a staff member to add $__ to your account and it worked!"];
+robpos = ["You broke into someone's house and stole his $__", "You rob a random passerby and manage to steal $__!", "You saw a account and seized the opportunity. You earned $__ from the heist!", "You hacked Echo and gave yourself $__", "You bribed a staff member to add $__ to your account and it worked!"];
 
 robneg = ["You try to rob a shop but got caught and fined $__", "You were caught vandalizing and got fined $__", "You hacked into the white house database but got caught. You lose $__", "You were caught stealing cookies and got fined $__", "You lost a bet against someone and lost $__"];
 
@@ -68,7 +68,7 @@ https://discord.gg/7JMJjGk\n\
 ```"
 
 //startsWith
-if (!String.prototype.startsWith) {
+if(!String.prototype.startsWith) {
     String.prototype.startsWith = function(search, pos) {
         return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
     };
@@ -76,11 +76,11 @@ if (!String.prototype.startsWith) {
 
 //Array.includes
 // https://tc39.github.io/ecma262/#sec-array.prototype.includes
-if (!Array.prototype.includes) {
+if(!Array.prototype.includes) {
     Object.defineProperty(Array.prototype, 'includes', {
         value: function(searchElement, fromIndex) {
 
-            if (this == null) {
+            if(this == null) {
                 throw new TypeError('"this" is null or not defined');
             }
 
@@ -91,7 +91,7 @@ if (!Array.prototype.includes) {
             var len = o.length >>> 0;
 
             // 3. If len is 0, return false.
-            if (len === 0) {
+            if(len === 0) {
                 return false;
             }
 
@@ -111,10 +111,10 @@ if (!Array.prototype.includes) {
             }
 
             // 7. Repeat, while k < len
-            while (k < len) {
+            while(k < len) {
                 // a. Let elementK be the result of ? Get(O, ! ToString(k)).
                 // b. If SameValueZero(searchElement, elementK) is true, return true.
-                if (sameValueZero(o[k], searchElement)) {
+                if(sameValueZero(o[k], searchElement)) {
                     return true;
                 }
                 // c. Increase k by 1. 
@@ -127,192 +127,188 @@ if (!Array.prototype.includes) {
     });
 }
 
-function getAccount(user) { 
-    if (user == undefined) {
+function getAccount(user) {
+    if(user == undefined) {
         username = RawUsername
-        user = RawUserID 
-    }
-    else {
-        username = undefined 
-        for (i in Server.Members) {
-            if (Server.Members[i].User.ID == user) {
-              username = Server.Members[i].User.Username
-            } 
-        } 
-        if (username == undefined) {
-          throw new ReferenceError("Invalid user passed into refreshAccount") 
-        } 
-    } 
-
-    if(!economy.hasOwnProperty(user)){
-        def = {    
-            name: username,
-             money: 0,
-             cooldown: 0   
+        user = RawUserID
+    } else {
+        username = undefined
+        for(i in Server.Members) {
+            if(Server.Members[i].User.ID == user) {
+                username = Server.Members[i].User.Username
+            }
         }
-        economy[user] = JSON.stringify(def) 
+        if(username == undefined) {
+            throw new ReferenceError("Invalid user passed into refreshAccount")
+        }
+    }
+
+    if(!economy.hasOwnProperty(user)) {
+        def = {
+            name: username,
+            money: 0,
+            cooldown: 0
+        }
+        economy[user] = JSON.stringify(def)
         return def
-    } 
+    }
     return JSON.parse(economy[user])
 }
 
 function getChannel(id) {
     guildChanID = [];
-    if (id == undefined) {
-        for (i = 0; i < Server.Channels; i++) {
+    if(id == undefined) {
+        for(i = 0; i < Server.Channels; i++) {
             guildChanID.push(Server.Channels[i].ID);
         }
         id = Channel.ID;
-    } 
-    if (channels.hasOwnProperty(id)) {   
+    }
+    if(channels.hasOwnProperty(id)) {
         channel = JSON.parse(channels[id]);
-        channel.name = Channel.Name; 
-    } 
-    else {
-        if (guildChanID.includes(channels[id])) {
+        channel.name = Channel.Name;
+    } else {
+        if(guildChanID.includes(channels[id])) {
             return getChannel(channels[Channel.ID]);
-        }   
-        channel = {     
+        }
+        channel = {
             crime: true,
             work: true,
             bal: true,
             prefix: "?",
-            name: Channel.Name   
+            name: Channel.Name
         }
     }
-    channels[Channel.ID] = JSON.stringify(channel); 
+    channels[Channel.ID] = JSON.stringify(channel);
     return channel;
 }
 
-function getRandomReply(invoked, money, check) { 
-    if (check == undefined) {   
-        check = "positive"; 
-    } 
-    if (check == "negative") {   
-        arr = robneg; 
-    } else {   
-        if (invoked == "crime") {     
-            arr = robpos;   
-        } else {     
-            if (invoked == "work") {       
-                arr = work;     
-            }   
-        } 
-    } 
+function getRandomReply(invoked, money, check) {
+    if(check == undefined) {
+        check = "positive";
+    }
+    if(check == "negative") {
+        arr = robneg;
+    } else {
+        if(invoked == "crime") {
+            arr = robpos;
+        } else {
+            if(invoked == "work") {
+                arr = work;
+            }
+        }
+    }
     return arr[Math.floor(Math.random() * arr.length)].replace("__", String(money));
 }
 
 function getGuildMemID() {
     list = [];
-    for (i in Server.Members) {
+    for(i in Server.Members) {
         list.push(Server.Members[i].User.ID);
     }
     return list;
 }
 
 function refreshAccount(user) {
-    if (user == undefined) {
+    if(user == undefined) {
         ID = RawUserID;
         name = RawUsername;
     } else {
-        username = undefined 
-        for (i in Server.Members) {
-            if (Server.Members[i].User.ID == user) {
-              username = Server.Members[i].User.Username
-            } 
-        } 
-        if (username == undefined) {
-          throw new ReferenceError("Invalid user passed into refreshAccount") 
-        } 
+        username = undefined
+        for(i in Server.Members) {
+            if(Server.Members[i].User.ID == user) {
+                username = Server.Members[i].User.Username
+            }
+        }
+        if(username == undefined) {
+            throw new ReferenceError("Invalid user passed into refreshAccount")
+        }
         ID = user
         name = username
     }
 
-    if (economy.hasOwnProperty(ID)) {   
+    if(economy.hasOwnProperty(ID)) {
         account = JSON.parse(economy[ID]);
         account.name = name
-    }
-    else {  
-        account = {    
+    } else {
+        account = {
             name: name,
-             money: 0,
-             cooldown: 0   
+            money: 0,
+            cooldown: 0
         }
-    } 
-    economy[ID] = JSON.stringify(account) 
+    }
+    economy[ID] = JSON.stringify(account)
     return;
 }
 
-function checkCooldown() { 
-    bank = JSON.parse(economy[RawUserID])
-    unix = Math.round((new Date()).getTime() / 1000); 
-    if (bank.cooldown < unix) {   
-        return false; 
-    } else {   
-        d = bank.cooldown - unix;   
-        h = Math.floor(d / 3600);   
-        m = Math.floor(d % 3600 / 60);   
-        s = Math.floor(d % 3600 % 60);   
-        hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";   
-        mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";   
+function checkCooldown() {
+    account = JSON.parse(economy[RawUserID]);
+    unix = Math.round((new Date()).getTime() / 1000);
+    if(account.cooldown < unix) {
+        return false;
+    } else {
+        d = account.cooldown - unix;
+        h = Math.floor(d / 3600);
+        m = Math.floor(d % 3600 / 60);
+        s = Math.floor(d % 3600 % 60);
+        hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+        mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
         sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-        Display = hDisplay + mDisplay + sDisplay;   
-        return Display.replace(new RegExp(",([^\,]*)$"), " and$1"); 
+        Display = hDisplay + mDisplay + sDisplay;
+        return Display.replace(new RegExp(",([^\,]*)$"), " and$1");
     }
 }
 
-function startCooldown(time) { 
-    bank = JSON.parse(economy[RawUserID]); 
-    unix = Math.round((new Date()).getTime() / 1000); 
-    bank.cooldown = unix + 60 * 60 * time; 
-    economy[RawUserID] = JSON.stringify(bank); 
+function startCooldown(time) {
+    account = JSON.parse(economy[RawUserID]);
+    unix = Math.round((new Date()).getTime() / 1000);
+    account.cooldown = unix + 60 * 60 * time;
+    economy[RawUserID] = JSON.stringify(account);
     return;
 }
 
-function editMoney(tare, value) { 
-    bank = JSON.parse(economy[RawUserID])
-    if (tare === 'positive') {   
-        bank.money += value; 
+function editMoney(tare, value) {
+    account = JSON.parse(economy[RawUserID])
+    if(tare === 'positive') {
+        account.money += value;
     } else
-    if (tare === 'negative') {   
-        bank.money -= value; 
-    } 
-    economy[RawUserID] = JSON.stringify(bank); 
+    if(tare === 'negative') {
+        account.money -= value;
+    }
+    economy[RawUserID] = JSON.stringify(account);
     return;
 }
 
-function checkEnabled(check) { 
-    channel = getChannel(); 
+function checkEnabled(check) {
+    channel = getChannel();
     return channel[check];
 }
 
-function editEnabled(change) { 
-    channel = getChannel(); 
-    channel[change] = !channel[change]; 
-    channels[Channel.ID] = JSON.stringify(channel); 
+function editEnabled(change) {
+    channel = getChannel();
+    channel[change] = !channel[change];
+    channels[Channel.ID] = JSON.stringify(channel);
     return;
 }
 
-function checkPrefix(invoked, pref) { 
-    if (pref == undefined) {   
-        return HasPrefix(Content, getChannel().prefix + invoked); 
-    } 
-    else {   
-        return HasPrefix(Content, pref + invoked); 
+function checkPrefix(invoked, pref) {
+    if(pref == undefined) {
+        return HasPrefix(Content, getChannel().prefix + invoked);
+    } else {
+        return HasPrefix(Content, pref + invoked);
     }
 }
 
 function getIcon(id) {
-    if (Channel.Type == 1) {
+    if(Channel.Type == 1) {
         return "https://cdn.discordapp.com/avatars/" + id + "/" + UserImage + ".png";
     }
     var text = [];
-    for (i = 0; i < ServerMembers.length; i++) {
-        if (ServerMembers[i].User.ID === id) {
+    for(i = 0; i < ServerMembers.length; i++) {
+        if(ServerMembers[i].User.ID === id) {
             text.push(ServerMembers[i].User.Avatar);
         }
     }
-    if (text.length !== 0) {
+    if(text.length !== 0) {
         var hash = text[0]
         return "https://cdn.discordapp.com/avatars/" + id + "/" + hash + ".png";
     } else {
@@ -320,33 +316,33 @@ function getIcon(id) {
     }
 }
 
-function formatEmbed(title, body, color, footer_obj) { 
-    emb = {}; 
-    emb.title = title; 
-    emb.description = body; 
-    emb.color = HTML2Int("#" + color); 
-    emb.author = {   
+function formatEmbed(title, body, color, footer_obj) {
+    emb = {};
+    emb.title = title;
+    emb.description = body;
+    emb.color = HTML2Int("#" + color);
+    emb.author = {
         name: RawUsername,
-           icon_url: getIcon(RawUserID) 
+        icon_url: getIcon(RawUserID)
     };
-    emb.footer = footer_obj; 
+    emb.footer = footer_obj;
     return emb;
 }
 
-function random(min, max) { 
+function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 function isStaff(UID) {
-    if (UID == undefined) {
+    if(UID == undefined) {
         UID = RawUserID;
     }
     addr = Address();
-    if (addr.OwnerID == UID) {
+    if(addr.OwnerID == UID) {
         return true;
     }
-    for (i = 0; i < addr.Staff.length; i++) {
-        if (addr.Staff[i] == UID) {
+    for(i = 0; i < addr.Staff.length; i++) {
+        if(addr.Staff[i] == UID) {
             return true;
         }
     }
@@ -357,35 +353,38 @@ String.prototype.isInteger = function() {
     return /^\d+$/.test(this);
 }
 
-function userToObject(obj){ 
-  //Given an ID or name, return a list of [ID, economy status]
-  if(obj == "<@" + UserID + ">"){
-    //There is a mention
-    if(economy.hasOwnProperty(UserID)){
-      return [UserID, economy[UserID]];
-    } else {
-      return undefined 
-    } 
-  } 
-  options = [] 
-  len = 0
-  for(i in economy){ 
-    len += 1
-    account = JSON.parse(economy[i]); 
-    if(i == obj){ 
-      options.push([i, economy[i]]) 
-    } 
-    if(account.name.startsWith(obj)){ 
-      options.push([i, economy[i]]) 
-    } 
-  } 
-  if(options.length == 1){ 
-    return options[0] ; 
-  } 
-  if(options.length == 0 || options.length == len){ 
-    return undefined; 
-  } 
-  return options; 
-} 
+function userToObject(obj) {
+    //Given an ID or name, return a list of [ID, economy status]
+    if(obj == "<@" + UserID + ">") {
+        //There is a mention
+        if(economy.hasOwnProperty(UserID)) {
+            return [UserID, economy[UserID]];
+        } else {
+            return undefined
+        }
+    }
+    options = []
+    len = 0
+    for(i in economy) {
+        len += 1
+        account = JSON.parse(economy[i]);
+        if(i == obj) {
+            options.push([i, economy[i]])
+        }
+        if(account.name.startsWith(obj)) {
+            options.push([i, economy[i]])
+        }
+    }
+    if(options.length == 1) {
+        return options[0];
+    }
+    if(options.length == 0 || options.length == len) {
+        return undefined;
+    }
+    return options;
+}
 
 refreshAccount();
+
+trigger = Trigger.replace("&", "").replace(" {params}", "")
+if(checkPrefix(trigger) || trigger == 'prefix'){
