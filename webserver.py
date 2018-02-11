5d4abed93d3c873c3ffeeaf9251df4cf436af880
fourjr <yeejiarong2004@gmail.com>
2018-02-11 15:02:40 +0800

    remove hastebin

--- webserver.py
+++ webserver.py
@@ -2,7 +2,6 @@ from japronto import Application
 import os
 import aiohttp 
 import aiofiles
-import base64 
 
 async def main(request):
     return request.Response(text='Pong')
@@ -18,16 +17,7 @@ async def get_file(request):
     except (FileNotFoundError, NotADirectoryError):
         return request.Response(text=f'No such file found: {fp}', code=404)
 
-async def hastebin(request):
-    '''POSTs to the Hastebin API'''
-    if app.session is None:
-        app.session = aiohttp.ClientSession(loop=app.loop)
-    async with app.session.post('https://www.hastebin.com/documents', data=base64.b64decode(request.query['data']).decode('utf-8'))as resp:
-        return request.Response(json=await resp.json())
-
 app = Application()
-app.session = None
 app.router.add_route('/', main)
-app.router.add_route('/hastebin', hastebin) 
 app.router.add_route('/file', get_file)
 app.run(port=int(os.getenv('PORT')))
